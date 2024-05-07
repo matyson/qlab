13edc2a6007a2358e3250a4acf6419aa3d0545ea
Matheus Luís <matheus.bernardi@lnls.br>
2024-05-07 10:26:13 -0300

    make attempts as hearts fading

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 import { cn } from './lib/utils';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 type Word = {
   word: string;
   definition: string;
   group: string;
 };
-
-// type Group = {
-//   name: string;
-//   words: Word[];
-// };
-
-// const groups: Group[] = [
-//   {
-//     name: "Fruit",
-//     words: [
-//       { word: "Apple", definition: "A fruit", group: "Fruit" },
-//       { word: "Banana", definition: "A fruit", group: "Fruit" },
-//     ],
-//   },
-//   {
-//     name: "Vegetable",
-//     words: [
-//       { word: "Carrot", definition: "A vegetable", group: "Vegetable" },
-//       { word: "Potato", definition: "A vegetable", group: "Vegetable" },
-//     ],
-//   },
-//   {
-//     name: "Animal",
-//     words: [
-//       { word: "Dog", definition: "An animal", group: "Animal" },
-//       { word: "Cat", definition: "An animal", group: "Animal" },
-//     ],
-//   },
-//   {
-//     name: "Vehicle",
-//     words: [
-//       { word: "Bus", definition: "A vehicle", group: "Vehicle" },
-//       { word: "Car", definition: "A vehicle", group: "Vehicle" },
-//     ],
-//   },
-//   {
-//     name: "Thing",
-//     words: [
-//       { word: "Book", definition: "A thing", group: "Thing" },
-//       { word: "Chair", definition: "A thing", group: "Thing" },
-//       { word: "Laptop", definition: "A thing", group: "Thing" },
-//       { word: "Table", definition: "A thing", group: "Thing" },
-//     ],
-//   },
-//   {
-//     name: "Color",
-//     words: [
-//       { word: "Red", definition: "A color", group: "Color" },
-//       { word: "Blue", definition: "A color", group: "Color" },
-//       { word: "Green", definition: "A color", group: "Color" },
-//       { word: "Yellow", definition: "A color", group: "Color" },
-//     ],
-//   },
-// ];
 
 const words: Word[] = [
   // 16 words divided equally in four distinct groups that make sense
@@ -99,13 +45,12 @@ const Card = ({ word, definition, group, select, selected }: CardProps) => {
 
   return (
     <motion.button
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: [null, 1.1, 1.05] }}
       whileTap={{ scale: 0.95 }}
-      exit={{ opacity: 0 }}
-      transition={{ ease: 'easeOut', duration: 1.1 }}
+      transition={{ ease: 'easeOut', duration: 0.6 }}
       layout
       className={cn(
-        'flex cursor-pointer flex-col items-center rounded-lg bg-amber-100 p-4 shadow-md hover:bg-amber-200',
+        'flex cursor-pointer flex-col items-center rounded-none bg-amber-100 p-4 shadow-md hover:bg-amber-200',
         selected && 'bg-pink-200 hover:bg-pink-300',
       )}
       onClick={handleClick}
@@ -124,24 +69,57 @@ type GroupProps = {
 
 const Group = ({ name, words }: GroupProps) => {
   return (
-    <div className="rounded-sm bg-gray-100 p-4 text-center shadow-md">
+    <motion.div
+      animate={{ scale: [0.5, 1.05, 1] }}
+      transition={{ ease: 'easeOut', duration: 1.2 }}
+      className="rounded-sm bg-orange-200 p-4 text-center shadow-md"
+    >
       <h2 className="text-xl font-bold">{name}</h2>
       <p className="text-md">
         {words.map((word, index) => (
-          <span key={index} className="text-white">
+          <span key={index}>
             {word.word}
             {index < words.length - 1 ? ', ' : ''}
           </span>
         ))}
       </p>
-    </div>
+    </motion.div>
   );
+};
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+    transition: { duration: 0.6 },
+  },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.5 },
+    scale: [0, 1.1, 1],
+  },
 };
 
 const Attempts = ({ attempts }: { attempts: number }) => {
   return (
-    <div>
-      <p>Attempts left: {attempts}</p>
+    <div className="flex">
+      <AnimatePresence mode="popLayout">
+        {Array.from({ length: attempts }).map((_, index) => (
+          <motion.span
+            initial={{
+              opacity: 0,
+              scale: 0,
+            }}
+            animate="visible"
+            exit="hidden"
+            variants={variants}
+            key={index}
+            className="text-2xl"
+          >
+            {'❤️'}
+          </motion.span>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
@@ -157,14 +135,16 @@ function App() {
   const [groupsFound, setGroupsFound] = useState<string[]>([]);
 
   useEffect(() => {
-    if (attempts === 0) {
-      alert('You lose!');
-      reset();
-    }
-    if (groupsFound.length === 4) {
-      alert('You win!');
-      reset();
-    }
+    setTimeout(() => {
+      if (attempts === 0) {
+        alert('You lose!');
+        reset();
+      }
+      if (groupsFound.length === 4) {
+        alert('You win!');
+        reset();
+      }
+    }, 0);
   }, [attempts, groupsFound.length]);
 
   const select = (word: Word) => {
@@ -214,7 +194,22 @@ function App() {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-amber-50">
-        <h1 className="fixed top-4 text-4xl font-bold">Q lab faz isso?</h1>
+        <a
+          href="https://pages.cnpem.br/cienciaaberta/"
+          target="_blank"
+          rel="noreferrer"
+          className="fixed bottom-4 right-4"
+        >
+          <img
+            src="//pages.cnpem.br/cienciaaberta/wp-content/uploads/sites/73/2023/04/id-04.png"
+            alt="ca-logo"
+            className="w-10"
+          />
+        </a>
+        <h1 className="fixed top-4 text-4xl font-bold text-[#f76c54]">
+          Q lab faz isso?
+        </h1>
+        <h2 className="text-xl font-semibold underline text-amber-500">Match the words</h2>
         <div className="grid grid-cols-4 gap-4 sm:w-screen md:w-4/5 lg:w-3/5">
           <div className="col-span-4 space-y-4">
             {groupsFound.map((group, index) => (
