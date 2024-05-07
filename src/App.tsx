b7f1cfae19ee87bc0c099aeb4fac8d630ff5ae67
Matheus Luís <matheus.bernardi@lnls.br>
2024-05-07 10:46:47 -0300

    add heart svg

--- src/App.tsx
+++ src/App.tsx
@@ -92,12 +92,27 @@ const variants = {
     opacity: 0,
     scale: 0,
     transition: { duration: 0.6 },
+    color: 'gray',
   },
   visible: {
     opacity: 1,
     transition: { duration: 0.5 },
     scale: [0, 1.1, 1],
+    color: 'red',
   },
+};
+
+const Heart = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="h-6 w-6"
+    >
+      <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+    </svg>
+  );
 };
 
 const Attempts = ({ attempts }: { attempts: number }) => {
@@ -116,7 +131,7 @@ const Attempts = ({ attempts }: { attempts: number }) => {
             key={index}
             className="text-2xl"
           >
-            {'❤️'}
+            <Heart />
           </motion.span>
         ))}
       </AnimatePresence>
@@ -209,7 +224,9 @@ function App() {
         <h1 className="fixed top-4 text-4xl font-bold text-[#f76c54]">
           Q lab faz isso?
         </h1>
-        <h2 className="text-xl font-semibold underline text-amber-500">Match the words</h2>
+        <h2 className="text-xl font-semibold text-amber-500 underline">
+          Match the words
+        </h2>
         <div className="grid grid-cols-4 gap-4 sm:w-screen md:w-4/5 lg:w-3/5">
           <div className="col-span-4 space-y-4">
             {groupsFound.map((group, index) => (
